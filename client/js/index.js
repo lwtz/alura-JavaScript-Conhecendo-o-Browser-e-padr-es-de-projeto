5aaaf9dd5f37d794c01c53b8abffe956c442e790
lwtz <w.h@outlook.com.br>
2021-08-02 09:54:32 -0300

    finish modulo 1

---
+++ client/js/index.js
@@ -0,0 +1,30 @@
+const campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+]
+let tbody = document.querySelector('table tbody')
+
+document.querySelector('.form').addEventListener('submit', event => {
+    event.preventDefault()
+    let tr = document.createElement('tr')
+
+    campos.forEach(campo => {
+        let td = document.createElement('td')
+        td.textContent = campo.value
+        tr.appendChild(td)
+    })
+
+
+    let tdVolume = document.createElement('td')
+    tdVolume.textContent = campos[1].value * campos[2].value
+    tr.appendChild(tdVolume)
+
+    tbody.appendChild(tr)
+
+    campos[0].value = ''
+    campos[1].value = 2
+    campos[2].value = 3
+    campos[0].focus()
+
+})
