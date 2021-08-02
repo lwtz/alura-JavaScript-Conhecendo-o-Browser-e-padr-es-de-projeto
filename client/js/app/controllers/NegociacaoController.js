b6b022fa868f2e9159a485f562dbc60213092195
lwtz <w.h@outlook.com.br>
2021-08-02 15:33:28 -0300

    Update Controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,5 +1,31 @@
 class NegociacaoController {
-    adiciona(event){
-        event.preventDefault()
+
+    constructor() {
+        let $ = document.querySelector.bind(document)
+        this._inputData = $('#data')
+        this._inputQuantidade = $('#quantidade')
+        this._inputValor = $('#valor')
     }
+
+    adiciona(event) {
+        event.preventDefault()
+        console.log(this._inputData.value)
+
+        let data = new Date(
+            ...this._inputData
+                .value
+                .split('-')
+                .map((item, index) =>
+                    item - index % 2
+                ))
+        console.log(data)
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+        console.log(negociacao)
+    }
+
 }
