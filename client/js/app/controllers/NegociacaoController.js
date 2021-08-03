747eb52bb7cd85ad24cc9ab2351f59050a769b87
lwtz <w.h@outlook.com.br>
2021-08-03 15:46:41 -0300

    alter name variables, and add mensagemview

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,39 +1,46 @@
 class NegociacaoController {
-
     /*
         Por padrão se nao for definido um constructor, ele vem vazio
-
         constructor() {}
-
      */
 
     constructor() {
         let $ = document.querySelector.bind(document)
-        this._inputData = $('#data')
-        this._inputQuantidade = $('#quantidade')
-        this._inputValor = $('#valor')
-        this._listaNegociacoes = new ListNegociacoes()
+        this._$inputData = $('#data')
+        this._$inputQuantidade = $('#quantidade')
+        this._$inputValor = $('#valor')
+
+        this._$listaNegociacoes = new ListNegociacoes()
+        this._$negociacoesView = new ViewNegociacoes($('#negociacoesView'))
+        this._$negociacoesView.update(this._$listaNegociacoes)
+
+        this.$_mensagem = new Mensagens()
+        this.$_mensagemView = new MensagemView($('#msgView'))
+        this.$_mensagemView.update(this.$_mensagem)
     }
 
     adiciona(event) {
         event.preventDefault()
-        this._listaNegociacoes.adiciona(this._criarNegociacao())
+        this._$listaNegociacoes.adiciona(this._criarNegociacao())
+        this._$negociacoesView.update(this._$listaNegociacoes)
+        this.$_mensagem.setTexto = 'Add com sucesso'
+        this.$_mensagemView.update(this.$_mensagem)
+
         this._limpaForm()
-        console.log(this._listaNegociacoes.negociacao)
     }
 
     _criarNegociacao() {
         return new Negociacao(
-            DateHelper.txtToData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
+            DateHelper.txtToData(this._$inputData.value),
+            this._$inputQuantidade.value,
+            this._$inputValor.value
         )
     }
 
     _limpaForm() {
-        this._inputData.value = ''
-        this._inputValor.value = 0
-        this._inputQuantidade = 0
-        this._inputData.focus()
+        this._$inputData.value = ''
+        this._$inputValor.value = 1
+        this._$inputQuantidade = 1
+        this._$inputData.focus()
     }
 }
